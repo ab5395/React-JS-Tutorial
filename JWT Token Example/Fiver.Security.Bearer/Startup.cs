9824800a58dc50cdcaa04dbee60f7b8a0e672a5d
bajay <bajay@C229>
2018-06-07 17:14:23 +0530

    Add Online Example Of Jwt Tokwn

---
+++ JWT Token Example/Fiver.Security.Bearer/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Fiver.Security.Bearer.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
+
+namespace Fiver.Security.Bearer
+{
+    public class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+
+                            ValidIssuer = "Fiver.Security.Bearer",
+                            ValidAudience = "Fiver.Security.Bearer",
+                            IssuerSigningKey = JwtSecurityKey.Create("fiver-secret-key")
+                        };
+
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnAuthenticationFailed = context =>
+                            {
+                                Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                                return Task.CompletedTask;
+                            },
+                            OnTokenValidated = context =>
+                            {
+                                Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                                return Task.CompletedTask;
+                            }
+                        };
+                    });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Member",
+                    policy => policy.RequireClaim("MembershipId"));
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
