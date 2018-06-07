69b19cacfc6ef1c1e0ca29e2d89028afbad0314f
bajay <bajay@C229>
2018-06-07 11:41:22 +0530

    Add Identity in .net core application

---
+++ IdentitySolution/IdentitySolution.Web/Models/SeedData.cs
@@ -0,0 +1,73 @@
+﻿using IdentitySolution.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySolution.Web.Models
+{
+    public class SeedData
+    {
+        private const string _adminRoleName = "Admin";
+        private string _adminEmail = "abh@narola.email";
+        private string _adminPassword = "123456";
+
+        private string[] _defaultRoles = new string[] { _adminRoleName, "Customer" };
+
+        private readonly RoleManager<IdentityRole<Guid>> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+        public static async Task Run(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var instance = serviceScope.ServiceProvider.GetService<SeedData>();
+                await instance.Initialize();
+            }
+        }
+
+        public SeedData(UserManager<User> userManager, RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task Initialize()
+        {
+            await EnsureRoles();
+            await EnsureDefaultUser();
+        }
+
+        protected async Task EnsureRoles()
+        {
+            foreach (var role in _defaultRoles)
+            {
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole<Guid>(role));
+                }
+            }
+        }
+
+        protected async Task EnsureDefaultUser()
+        {
+            var adminUsers = await _userManager.GetUsersInRoleAsync(_adminRoleName);
+
+            if (!adminUsers.Any())
+            {
+                var adminUser = new User()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Email = _adminEmail,
+                    UserName = _adminEmail
+                };
+
+                var result = await _userManager.CreateAsync(adminUser, _adminPassword);
+                await _userManager.AddToRoleAsync(adminUser, _adminRoleName);
+            }
+        }
+
+    }
+}
